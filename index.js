ae18a25c6d8f761a05ba91bce5a16ffcba6cd823
devin-gar47 <devingar18@gmail.com>
2022-03-27 18:26:21 -0500

    update

--- index.js
+++ index.js
@@ -16,13 +16,6 @@ const corsOptions = {
   credentials: true
 }
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-
 app.use(cors(corsOptions))
 app.use(express.json());
 app.use(express.urlencoded({
